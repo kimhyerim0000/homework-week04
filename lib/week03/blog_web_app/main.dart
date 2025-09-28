1a033f01e84c45dccc542dc88508c9129eda43af
kimhy <kimhyyefla@gmail.com>
2025-09-29 02:34:35 +0900

    feat: Ensure Flutter bindings are initialized

--- lib/week03/blog_web_app/main.dart
+++ lib/week03/blog_web_app/main.dart
@@ -2,6 +2,7 @@ import 'package:myapp/week03/blog_web_app/screen/home_screen.dart';
 import 'package:flutter/material.dart';
 
 void main() {
+  WidgetsFlutterBinding.ensureInitialized();
   runApp(
     MaterialApp(
       home: HomeScreen(),
