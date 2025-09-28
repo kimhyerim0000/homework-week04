720ee809a8397c9b92d903805ac78da0293fb8bf
kimhy <kimhyyefla@gmail.com>
2025-09-29 02:45:34 +0900

    feat: Implement basic image carousel

--- lib/week03/image_carousel/screen/home_screen.dart
+++ lib/week03/image_carousel/screen/home_screen.dart
@@ -7,7 +7,11 @@ class HomeScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: PageView(
-        
+        children: [1,2,3,4,5]
+         .map(
+        (number) => Image.asset('asset/img/image_$number.jpeg'),
+         )
+         .toList(),
       )
     );
   }
