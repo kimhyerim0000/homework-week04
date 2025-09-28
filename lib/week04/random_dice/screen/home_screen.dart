feae632e25db3bff2c5640c3eb590610b6fe777a
kimhy <kimhyyefla@gmail.com>
2025-09-29 03:43:05 +0900

    feat(dice): Decrement firstDay on heart press

--- lib/week04/random_dice/screen/home_screen.dart
+++ lib/week04/random_dice/screen/home_screen.dart
@@ -37,7 +37,12 @@ class _HomeScreenState extends State<HomeScreen> {
 }
 
 void onHeartPressed() { // 하트 눌렀을 때 실행할 함수
-  print('클릭');
+  // 상태 변경 시 setState() 함수 실행
+  setState(() {
+
+    // firstDay 변수에서 하루 빼기
+    firstDay = firstDay.subtract(Duration(days: 1));
+  });
 }
 
 
