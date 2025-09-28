0cc00995301298e37b2740c425b0889776e0ef24
kimhy <kimhyyefla@gmail.com>
2025-09-29 02:28:04 +0900

    feat: Add AppBar to HomeScreen with title and styling.

--- lib/week03/blog_web_app/screen/home_screen.dart
+++ lib/week03/blog_web_app/screen/home_screen.dart
@@ -9,7 +9,11 @@ class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      
+      appBar: AppBar(
+        backgroundColor: Colors.orange,
+        title: Text('Code Factory'),
+        centerTitle: true,
+      )
     );
   }
 }
