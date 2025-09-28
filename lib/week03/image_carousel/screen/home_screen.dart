94606ac6f35df1336048a32780f3c574225fbbd8
kimhy <kimhyyefla@gmail.com>
2025-09-29 02:44:33 +0900

    Updated Changes

--- lib/week03/image_carousel/screen/home_screen.dart
+++ lib/week03/image_carousel/screen/home_screen.dart
@@ -6,7 +6,9 @@ class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Text('Home Screen'),
+      body: PageView(
+        
+      )
     );
   }
 }
