25298b89d264d9dd3bdb410349824a082c28188f
kimhy <kimhyyefla@gmail.com>
2025-09-29 02:56:05 +0900

    feat(home_screen): Implement initState for future initialization

--- lib/week03/image_carousel/screen/home_screen.dart
+++ lib/week03/image_carousel/screen/home_screen.dart
@@ -12,6 +12,11 @@ class HomeScreen extends StatefulWidget {
 // _HomeScreenState 정의 
 class _HomeScreenState extends State<HomeScreen> {
 
+  // initState() 함수 등록
+  @override
+  void initState() {
+    super.initState();
+  }
   @override
   Widget build(BuildContext context) {
     // 상태바 색상 변경
